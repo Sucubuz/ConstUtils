3697169fed5adbb2fc3e90956a00db13c6c32e80
cutebear0123 <cutebear@0w0b.dev>
2025-06-01 21:58:41 -0700

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# LoliHack
+An addon for meteor client 1.21.5 made by breadableloli for constantiam
+
+I provide no support, glhf
---
+++ src/main/java/breedableloli/lolihack/LoliHackAddon.java
@@ -0,0 +1,48 @@
+package breedableloli.lolihack;
+
+import breedableloli.lolihack.modules.*;
+//import com.example.addon.hud.HudExample;
+//import com.example.addon.modules.ModuleExample;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class LoliHackAddon extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("LoliHack");
+    public static final HudGroup HUD_GROUP = new HudGroup("LoliHack");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Addon Template");
+        Modules modules = Modules.get();
+        // Modules
+        modules.add(new ElytraFlyPlus());
+        modules.add(new InfiniteWeapons());
+        modules.add(new LagNotifier());
+        modules.add(new AutoOmen());
+        modules.add(new ConstAntiQueue());
+        // Commands
+        // Commands.add(new CommandExample());
+
+        // HUD
+        // Hud.get().register(HudExample.INFO);
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "breedableloli.lolihack";
+    }
+
+}
